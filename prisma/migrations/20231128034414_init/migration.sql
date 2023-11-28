a037c6cbdf13b7f54196acd4d197e82148541d66
Julian Fragoso <starsmember@yahoo.com>
2023-11-27 21:47:51 -0600

    added villain model

--- prisma/migrations/20231128034414_init/migration.sql
+++ prisma/migrations/20231128034414_init/migration.sql
@@ -1,7 +1,18 @@
 -- CreateTable
+CREATE TABLE "Villain" (
+    "villainName" TEXT NOT NULL,
+    "realName" TEXT NOT NULL,
+    "partnerships" TEXT NOT NULL,
+
+    CONSTRAINT "Villain_pkey" PRIMARY KEY ("villainName")
+);
+
+-- CreateTable
 CREATE TABLE "Company" (
     "companyId" TEXT NOT NULL,
     "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Company_pkey" PRIMARY KEY ("companyId")
 );
@@ -11,18 +22,28 @@ CREATE TABLE "Department" (
     "departmentId" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "companyId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Department_pkey" PRIMARY KEY ("departmentId")
 );
 
 -- CreateTable
 CREATE TABLE "Employee" (
-    "employeeID" TEXT NOT NULL,
+    "employeeId" TEXT NOT NULL,
     "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
     "departmentId" TEXT NOT NULL,
+    "quote" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Employee_pkey" PRIMARY KEY ("employeeID")
+    CONSTRAINT "Employee_pkey" PRIMARY KEY ("employeeId")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Employee_email_key" ON "Employee"("email");
 
 -- AddForeignKey
 ALTER TABLE "Department" ADD CONSTRAINT "Department_companyId_fkey" FOREIGN KEY ("companyId") REFERENCES "Company"("companyId") ON DELETE RESTRICT ON UPDATE CASCADE;
